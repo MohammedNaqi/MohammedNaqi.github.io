96bf1eb6e428643ec3833c1a0f166a383035f9bd
MohammedNaqi <mohammed.syed1@dcmail.ca>
2023-11-17 18:58:01 -0800

    second if statement
    Used the replace for temperature and weight

--- Assignment4 JS/Assignment4(A)/main.js
+++ Assignment4 JS/Assignment4(A)/main.js
@@ -25,7 +25,8 @@ function result() {
   if(document.getElementById("uk").checked) {
     const weight = Math.round(300 / 14) + 'Stone';
     const temperature =  Math.round(94 - 32 * 5/9) + centigrade;
- 
+    newStory = newStory.replace("94 fahrenheit", temperature)
+    newStory = newStory.replace("300 pounds", weight)
   }
 
   story.textContent = newStory;
