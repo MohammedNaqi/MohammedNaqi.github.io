875543d7322d3e01a90459bf523e64933b0b910e
MohammedNaqi <mohammed.syed1@dcmail.ca>
2023-11-20 16:16:26 -0800

    Arrays
    Two arrays of images names and alt text for the images

--- Assignment4 JS/Assihnment4(B)/gallery-start/main.js
+++ Assignment4 JS/Assihnment4(B)/gallery-start/main.js
@@ -5,14 +5,19 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imagesArray = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg']
 
 /* Declaring the alternative text for each image file */
-
+const altImage = ['This is a photo ofa portion of someone face','A rock formation', 'purple flowers', 'Ancient Ejyptian murals','A butterfly on the leaf' ]
 /* Looping through images */
 
-const newImage = document.createElement('img');
+for (const image of imagesArray){
+    const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
 newImage.setAttribute('alt', xxx);
 thumbBar.appendChild(newImage);
+}
+
+
 
 /* Wiring up the Darken/Lighten button */
