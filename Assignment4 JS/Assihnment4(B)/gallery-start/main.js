18d0927343a17d0a8d55c33ba18a3730f30de99c
MohammedNaqi <mohammed.syed1@dcmail.ca>
2023-11-20 16:24:11 -0800

    Loop
    Made a for of loop for the images

--- Assignment4 JS/Assihnment4(B)/gallery-start/main.js
+++ Assignment4 JS/Assihnment4(B)/gallery-start/main.js
@@ -5,16 +5,18 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const imagesArray = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg']
+const imagesArray = ['images/pic1.jpg','images/pic2.jpg','images/pic3.jpg','images/pic4.jpg','images/pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
-const altImage = ['This is a photo ofa portion of someone face','A rock formation', 'purple flowers', 'Ancient Ejyptian murals','A butterfly on the leaf' ]
+const altImage = ['This is a photo ofa portion of someone face','A rock formation', 'purple flowers', 'Ancient Ejyptian murals','A butterfly on the leaf' ];
 /* Looping through images */
 
 for (const image of imagesArray){
     const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
+    const altText = altImage[0]
+
+newImage.setAttribute('src', image);
+newImage.setAttribute('alt', altText);
 thumbBar.appendChild(newImage);
 }
 
