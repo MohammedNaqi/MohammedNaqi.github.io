071aceb97af1f6039eb0ec84a5df56d198bce7ec
MohammedNaqi <mohammed.syed1@dcmail.ca>
2023-11-21 18:30:34 -0800

    Adding click event listener

--- Assignment4 JS/Assihnment4(B)/gallery-start/main.js
+++ Assignment4 JS/Assihnment4(B)/gallery-start/main.js
@@ -5,21 +5,46 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const imagesArray = ['images/pic1.jpg','images/pic2.jpg','images/pic3.jpg','images/pic4.jpg','images/pic5.jpg'];
+const imagesArray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
-const altImage = ['This is a photo ofa portion of someone face','A rock formation', 'purple flowers', 'Ancient Ejyptian murals','A butterfly on the leaf' ];
+const altText = {
+    'pic1.jpg':'photo of a portion of someone face', 
+    'pic2.jpg':'A rock formation',
+    'pic3.jpg':'purple flowers', 
+    'pic4.jpg':'Ancient Ejyptian murals',
+    'pic5.jpg':'A butterfly on the leaf'
+};
 /* Looping through images */
 
-for (const image of imagesArray){
+for (const image of imagesArray) {
     const newImage = document.createElement('img');
-    const altText = altImage[0]
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt', altText[image]);
+    thumbBar.appendChild(newImage);
+    // Adding a click event listener to each thumbnail image
+    newImage.addEventListener("click", e => {
+        displayedImage.src = e.target.src;
+        displayedImage.alt = e.target.alt;
+})};
 
-newImage.setAttribute('src', image);
-newImage.setAttribute('alt', altText);
-thumbBar.appendChild(newImage);
-}
+// Adding a click event listener to each thumbnail image
 
 
+// function button (newImage, altText){
+//     newImage.setAttribute('src', newImage.scr);
+//     newImage.setAttribute('alt', altText);
+// }
 
 /* Wiring up the Darken/Lighten button */
+
+// const btnClass = btn.getAttribute('class')
+
+// function dark 
+// if (btnClass === "dark") {
+//     btn.setAttribute = 
+// }
+
+// btn.setAttribute("class", xxx);
+// btn.textContent = xxx;
+// overlay.style.backgroundColor = xxx;
