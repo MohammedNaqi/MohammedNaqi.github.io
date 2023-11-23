dbff41941cf1e7aa224d3502530f1f4689e695c4
MohammedNaqi <mohammed.syed1@dcmail.ca>
2023-11-22 22:56:28 -0800

    EvilCircle constructor

--- Assignment4JS/Assignment4(D)/main-finished.js
+++ Assignment4JS/Assignment4(D)/main-finished.js
@@ -5,12 +5,14 @@ const ctx = canvas.getContext('2d');
 
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
+const para =   document.querySelector('p')
 
 let count = 0
 // function to generate random number
 
 function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+   const num = Math.floor(Math.random()*(max-min)) + min;
+   return num;
 }
 
 // function to generate random RGB color value
@@ -81,8 +83,26 @@ class Ball extends Shape {
 class Evilcircle {
    constructor(x,y){
       super(x, y, 20, 20);
+
       this.color ="white";
       this.size = 10;
+
+      window.addEventListener("keydown", (e) => {
+         switch (e.key) {
+           case "a":
+             this.x -= this.velX;
+             break;
+           case "d":
+             this.x += this.velX;
+             break;
+           case "w":
+             this.y -= this.velY;
+             break;
+           case "s":
+             this.y += this.velY;
+             break;
+         }
+       });
    }
    draw() {
       ctx.beginPath();
@@ -138,11 +158,15 @@ while (balls.length < 25) {
       random(-7,7),
       random(-7,7),
       randomRGB(),
-      size
+      size 
    );
 
   balls.push(ball);
+  count++;
+  para.textContent = 'Ball count: ' + count;
 }
+
+const Evilblob = new Evilcircle(random(0, width), random(0, height));
 
 function loop() {
    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
