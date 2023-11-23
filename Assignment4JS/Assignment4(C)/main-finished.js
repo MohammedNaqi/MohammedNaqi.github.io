eeec3e3d9f7b7a22d6b7356c0130d37d4ae79d8c
MohammedNaqi <mohammed.syed1@dcmail.ca>
2023-11-22 18:46:18 -0800

    Animation

--- Assignment4JS/Assignment4(C)/main-finished.js
+++ Assignment4JS/Assignment4(C)/main-finished.js
@@ -75,3 +75,14 @@ const balls = [];
  
    balls.push(ball);
  }
+function loop() {
+   ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+   ctx.fillRect(0, 0, width, height);
+ 
+   for (const ball of balls) {
+     ball.draw();
+     ball.update();
+   }
+ 
+   requestAnimationFrame(loop);
+ }
