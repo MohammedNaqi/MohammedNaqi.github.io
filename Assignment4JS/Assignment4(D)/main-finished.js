7e1583af1fd356058e20e3b98f17a8aec9bf4d3f
MohammedNaqi <mohammed.syed1@dcmail.ca>
2023-11-22 20:11:47 -0800

    Draw
    Add the Draw method to the evilcircle class

--- Assignment4JS/Assignment4(D)/main-finished.js
+++ Assignment4JS/Assignment4(D)/main-finished.js
@@ -83,6 +83,13 @@ class Evilcircle {
       this.color ="white";
       this.size = 10;
    }
+   draw() {
+      ctx.beginPath();
+      ctx.lineWidth = 3 
+      ctx.strokeStyle = this.color;
+      ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+      ctx.stroke();
+   }
 }
 
 const balls = [];
